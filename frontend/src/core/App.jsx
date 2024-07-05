4a9bf228161511deaad4a5fbedb0d68cd637f9b9
Nizamul Kazi <nz707888@dal.ca>
2024-07-05 18:35:55 -0300

    feat: init q&a section

--- frontend/src/core/App.jsx
+++ frontend/src/core/App.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 /* styles */
 import './index.css';
 /* internal components */
-import { Link } from 'react-router-dom';
 import HomePage from '../components/pages/HomePage';
 
 function App() {
   return (
     <HomePage />
-
   );
 }
 
